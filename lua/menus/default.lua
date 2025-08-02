688bca64e9c208404c6a703f27769ed04f1e584d
量元工作室 <103371145+quanmeta@users.noreply.github.com>
2025-08-02 16:06:20 +0800

    Update default.lua

--- lua/menus/default.lua
+++ lua/menus/default.lua
@@ -1,29 +1,23 @@
 return {
 
-  {
-    name = "Format",
-    cmd = function()
-      local ok, conform = pcall(require, "conform")
+  -- {
+  --   name = "Format",
+  --   cmd = function()
+  --     local ok, conform = pcall(require, "conform")
+  -- 
+  --     if ok then
+  --       conform.format { lsp_fallback = true }
+  --     else
+  --       vim.lsp.buf.format()
+  --     end
+  --   end,
+  --   rtxt = ",F",
+  -- },
 
-      if ok then
-        conform.format { lsp_fallback = true }
-      else
-        vim.lsp.buf.format()
-      end
-    end,
-    rtxt = ",F",
-  },
-
-  {
-    name = "Code Actions",
-    cmd = vim.lsp.buf.code_action,
-    rtxt = "<leader>ca",
-  },
-
-  { name = "separator" },
+  -- { name = "separator" },
 
   {
-    name = "  Lsp Actions",
+    name = "  Lsp",
     hl = "Exblue",
     items = "lsp",
   },
@@ -32,28 +26,56 @@ return {
 
   {
     name = "Copy",
+    cmd = "y +",
+    rtxt = "y",
+  },
+
+  {
+    name = "Copy All",
     cmd = "%y+",
-    rtxt = "<C-c>",
+  },
+
+  {
+    name = "Paste",
+    cmd = function()
+      local mode = vim.fn.mode()
+      if mode == "n" then
+        vim.cmd([[
+          normal! mz"_dd`zP
+        ]])
+      elseif mode == "v" or mode == "x" or mode == "\22" then
+        vim.cmd('normal! "_dP')
+      end
+    end,
+    rtxt = "p",
   },
 
   {
     name = "Delete",
-    cmd = "%d",
-    rtxt = "dc",
+    cmd = 'normal! "_x',
+    rtxt = "<delete>",
   },
 
   { name = "separator" },
 
   {
-    name = "  Open in terminal",
+    name = "  Terminal",
     hl = "ExRed",
-    cmd = vim.cmd( "ToggleTerm direction=vertical" ),
+    cmd = "ToggleTerm direction=vertical",
+  },
+
+  {
+    name = "  Explorer",
+    hl = "ExRed",
+    cmd = function()
+        Snacks.explorer({ cwd = LazyVim.root() })
+    end,
   },
 
   { name = "separator" },
 
   {
-    name = "  Color Picker",
+    name = "  Color",
     cmd = function()
       require("minty.huefy").open()
     end,
